70f4d2dfa03e01fb2366cced52ab12448cb22eb4
Hank <hank.likes.matcha@gmail.com>
2020-06-08 21:31:36 +1200

    add chart

--- lib/components/chart/view/chart.dart
+++ lib/components/chart/view/chart.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-class Chart extends StatelessWidget {
-  const Chart({
-    Key key,
-  }) : super(key: key);
+class Chart extends StatefulWidget {
+  @override
+  _ChartState createState() => _ChartState();
+}
+
+class _ChartState extends State<Chart> {
+  bool showAvg = false;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
+        child: LineChart(showAvg ? avgData() : mainData()),
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           color: Colors.black,
@@ -17,4 +22,193 @@ class Chart extends StatelessWidget {
       ),
     );
   }
+}
+
+LineChartData mainData() {
+  return LineChartData(
+    gridData: FlGridData(
+      show: true,
+      drawVerticalLine: true,
+      getDrawingHorizontalLine: (value) {
+        return FlLine(
+          color: const Color(0xff37434d),
+          strokeWidth: 1,
+        );
+      },
+      getDrawingVerticalLine: (value) {
+        return FlLine(
+          color: const Color(0xff37434d),
+          strokeWidth: 1,
+        );
+      },
+    ),
+    titlesData: FlTitlesData(
+      show: true,
+      bottomTitles: SideTitles(
+        showTitles: true,
+        reservedSize: 22,
+        textStyle: const TextStyle(
+            color: Color(0xff68737d),
+            fontWeight: FontWeight.bold,
+            fontSize: 16),
+        getTitles: (value) {
+          switch (value.toInt()) {
+            case 2:
+              return 'MAR';
+            case 5:
+              return 'JUN';
+            case 8:
+              return 'SEP';
+          }
+          return '';
+        },
+        margin: 8,
+      ),
+      leftTitles: SideTitles(
+        showTitles: true,
+        textStyle: const TextStyle(
+          color: Color(0xff67727d),
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+        getTitles: (value) {
+          switch (value.toInt()) {
+            case 1:
+              return '10k';
+            case 3:
+              return '30k';
+            case 5:
+              return '50k';
+          }
+          return '';
+        },
+        reservedSize: 28,
+        margin: 12,
+      ),
+    ),
+    borderData: FlBorderData(
+        show: true,
+        border: Border.all(color: const Color(0xff37434d), width: 1)),
+    minX: 0,
+    maxX: 11,
+    minY: 0,
+    maxY: 6,
+    lineBarsData: [
+      LineChartBarData(
+        spots: [
+          FlSpot(0, 3),
+          FlSpot(2.6, 2),
+          FlSpot(4.9, 5),
+          FlSpot(6.8, 3.1),
+          FlSpot(8, 4),
+          FlSpot(9.5, 3),
+          FlSpot(11, 4),
+        ],
+        isCurved: true,
+        barWidth: 5,
+        isStrokeCapRound: true,
+        dotData: FlDotData(
+          show: false,
+        ),
+        belowBarData: BarAreaData(
+          show: true,
+        ),
+      ),
+    ],
+  );
+}
+
+LineChartData avgData() {
+  return LineChartData(
+    lineTouchData: LineTouchData(enabled: false),
+    gridData: FlGridData(
+      show: true,
+      drawHorizontalLine: true,
+      getDrawingVerticalLine: (value) {
+        return FlLine(
+          color: const Color(0xff37434d),
+          strokeWidth: 1,
+        );
+      },
+      getDrawingHorizontalLine: (value) {
+        return FlLine(
+          color: const Color(0xff37434d),
+          strokeWidth: 1,
+        );
+      },
+    ),
+    titlesData: FlTitlesData(
+      show: true,
+      bottomTitles: SideTitles(
+        showTitles: true,
+        reservedSize: 22,
+        textStyle: const TextStyle(
+            color: Color(0xff68737d),
+            fontWeight: FontWeight.bold,
+            fontSize: 16),
+        getTitles: (value) {
+          switch (value.toInt()) {
+            case 2:
+              return 'MAR';
+            case 5:
+              return 'JUN';
+            case 8:
+              return 'SEP';
+          }
+          return '';
+        },
+        margin: 8,
+      ),
+      leftTitles: SideTitles(
+        showTitles: true,
+        textStyle: const TextStyle(
+          color: Color(0xff67727d),
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+        getTitles: (value) {
+          switch (value.toInt()) {
+            case 1:
+              return '10k';
+            case 3:
+              return '30k';
+            case 5:
+              return '50k';
+          }
+          return '';
+        },
+        reservedSize: 28,
+        margin: 12,
+      ),
+    ),
+    borderData: FlBorderData(
+        show: true,
+        border: Border.all(color: const Color(0xff37434d), width: 1)),
+    minX: 0,
+    maxX: 11,
+    minY: 0,
+    maxY: 6,
+    lineBarsData: [
+      LineChartBarData(
+        spots: [
+          FlSpot(0, 3.44),
+          FlSpot(2.6, 3.44),
+          FlSpot(4.9, 3.44),
+          FlSpot(6.8, 3.44),
+          FlSpot(8, 3.44),
+          FlSpot(9.5, 3.44),
+          FlSpot(11, 3.44),
+        ],
+        isCurved: true,
+        barWidth: 5,
+        isStrokeCapRound: true,
+        dotData: FlDotData(
+          show: false,
+        ),
+        belowBarData: BarAreaData(
+          show: true,
+        ),
+      ),
+    ],
+  );
 }
